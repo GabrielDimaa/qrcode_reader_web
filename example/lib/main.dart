ea5c420e0fed7581f9752ff531fc91635ce9a726
Gabriel de Matos <gabrielmatos1999@rede.ulbra.br>
2023-05-09 09:30:24 -0300

    Example app

---
+++ example/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:qrcode_reader_web/qrcode_reader_web.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<QRCodeCapture> list = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Example App"),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            QRCodeReaderWidget(
+              onDetect: (QRCodeCapture capture) => setState(() => list.add(capture)),
+              size: 350,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: list.length,
+                itemBuilder: (_, index) {
+                  return ListTile(title: Text(list[index].raw));
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
